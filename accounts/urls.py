fda83713a290d19bcac01d374d777e873b213402
sangbaek <aumsbk@naver.com>
2022-08-29 03:08:18 +0900

    Feat : #3 토큰 인증을 기반으로 한 로그인 API 개발 - 회원가입된 정보로 로그인 시, 유저에게 access token과 refresh token 발급 - accounts앱에 SignInView 및 SignInSerializer 생성 - access token 및 refresh token이 만료될 경우, 재발급 할 수 있게 해주는 API 개발 - simplejwt의 TokenRefreshView 이용

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from accounts.views import SignUpView
+from accounts.views import SignInView, SignUpView
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("v1/accounts/signup", SignUpView.as_view()),
+    path("v1/users/signup", SignUpView.as_view()),
+    path("v1/users/signin", SignInView.as_view()),
+    path("v1/token/refresh", TokenRefreshView.as_view()),
 ]
